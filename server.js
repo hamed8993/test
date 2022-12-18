3d683d1659f4b8cbe8807716ca7ad1d4ff8439b7
hamed <95145548+hamed8993@users.noreply.github.com>
2022-12-18 22:39:50 +0330

    erase added things & returned original state

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const app = express();
-app.use(express.static("./front"));
 const http = require("http");
-const path = require("path");
-app.use(express.static(path.join(__dirname, '/front')));
 
 const server = http.createServer(app);
 const { Server } = require("socket.io");
